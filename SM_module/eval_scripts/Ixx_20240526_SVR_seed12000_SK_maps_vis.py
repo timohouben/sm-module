9cc87b02e0f0ed439e77018fb0eb9624fa45c65e
Timo Houben <timo.houben@ufz.de>
2024-05-26 17:17:39 +0200

    added evaluation script for SVR

---
+++ SM_module/eval_scripts/Ixx_20240526_SVR_seed12000_SK_maps_vis.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+# Authors:  Timo Houben <timo.houben@ufz.de>
+# ------------------------------------------------------------------------------
+import os
+from sklearn.neural_network import MLPRegressor
+
+# own modules
+from SM.maps import SpatialMap
+from SM.training import SpatioTempModel
+from SM.io import load_model
+
+# ------------------------------------------------------------------------------
+# USER SETTINGS
+# ------------------------------------------------------------------------------
+# mandatory variables
+name = "SK"
+method = "iter_5000_seed_12000"
+details = "seed"
+# Specify the path to the project directory containing the model to load
+# subfolder called 'models' and where results will be saved
+project_dir = "/work/houben/20240322-ml-cafe/rerun-air_temp"
+# specify the model and its parameters
+estimator = MLPRegressor()
+# define features to use
+features = None  # if None, default is used
+
+# ------------------------------------------------------------------------------
+# LOAD MODEL
+# ------------------------------------------------------------------------------
+fname = os.path.join(project_dir, "models", name, method, name + "_" + method)
+st_model = SpatioTempModel(
+    estimator=estimator,
+    method=method,
+    name=name,
+    project_dir=project_dir,
+    details=details,
+    features=features,
+)
+
+# load the model and save it as attribute of the
+st_model.model, _ = load_model(fname)
+
+# ------------------------------------------------------------------------------
+# MAP CREATION
+# ------------------------------------------------------------------------------
+# create maps for several dates
+# date for which the maps should be predicted and plotted
+dates = [
+    "2012-09-16",
+    "2012-10-16",
+    "2012-11-16",
+    "2012-12-16",
+    "2013-01-16",
+    "2013-02-16",
+    "2013-03-16",
+    "2013-04-16",
+    "2013-05-16",
+    "2013-06-16",
+    "2013-07-16",
+    "2013-08-16",
+    "2013-09-16",
+    "2013-10-16",
+]
+
+for date in dates:
+    print("Predicting map for method", method, "date", date)
+    # predict the map
+    map_3d = SpatialMap(name=name, method=method, date=date)
+    # plot the map
+    map_3d.plot_maps(grid=False)
+    # save the predicted map as csv file
+    # map_3d.save_csv()
